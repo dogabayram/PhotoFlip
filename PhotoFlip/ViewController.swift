dd87c9590aa569e19edb7d4a59b2b75bf3420730
dogabayram <dogabayram33@hotmail.com.tr>
2018-12-25 13:44:47 +0100

    Update

--- PhotoFlip/ViewController.swift
+++ PhotoFlip/ViewController.swift
@@ -7,11 +7,13 @@
 //
 
 import UIKit
+import MobileCoreServices
+import GoogleMobileAds
 
-class ViewController: UIViewController , UIImagePickerControllerDelegate,UINavigationControllerDelegate {
+class ViewController: UIViewController , UIImagePickerControllerDelegate,UINavigationControllerDelegate,GADBannerViewDelegate ,GADInterstitialDelegate{
     
     
-    @IBOutlet weak var bannerView: UIView!
+    @IBOutlet weak var bannerView: GADBannerView!
     @IBOutlet weak var imageView: UIImageView!
     var originalHorizonFlip = true
     var originalVertiaclFlip = true
@@ -19,6 +21,10 @@ class ViewController: UIViewController , UIImagePickerControllerDelegate,UINavig
     var imageMirror = UIImage()
     @IBOutlet weak var choosePhotoLabel: UILabel!
     let imagePicker = UIImagePickerController()
+    var interstitial: GADInterstitial!
+    var fromGallery : Bool?
+    @IBOutlet weak var chooseButton: UIButton!
+    
 
     
  
@@ -26,12 +32,28 @@ class ViewController: UIViewController , UIImagePickerControllerDelegate,UINavig
         super.viewDidLoad()
         self.imageView.isUserInteractionEnabled = true
         imagePicker.delegate = self
+        bannerFunc()
 
        //originalImage = UIImage(named: "Mersin-Gezilecek-Yerler")
        //imageView.image = originalImage
     
        swipeGestures()
         
+    }
+    
+    func bannerFunc() {
+        
+        bannerView.rootViewController = self
+        bannerView.load(GADRequest())
+        bannerView.delegate = self
+
+    }
+    
+    func interstitialAds() {
+        
+        interstitial.delegate = self
+        let request = GADRequest()
+        interstitial.load(request)
     }
     
     func swipeGestures () {
@@ -54,15 +76,16 @@ class ViewController: UIViewController , UIImagePickerControllerDelegate,UINavig
     
     
     @IBAction func saveImageButton(_ sender: UIButton) {
-        
+     
         if originalImage != nil {
-        UIImageWriteToSavedPhotosAlbum(imageMirror, self, #selector(self.cameraRollComplete(_:finishedSavingWithError:contextInfo:)), nil)
+                UIImageWriteToSavedPhotosAlbum(imageMirror, self, #selector(self.cameraRollComplete(_:finishedSavingWithError:contextInfo:)), nil)
+            
+          
         } else {
             alert()
         }
         
-        
-        
+  
     }
     @objc func cameraRollComplete(_ image: UIImage, finishedSavingWithError error: NSError, contextInfo: UnsafeMutableRawPointer) {
         let alert = UIAlertController(
@@ -71,54 +94,59 @@ class ViewController: UIViewController , UIImagePickerControllerDelegate,UINavig
             message: nil,
             preferredStyle: .alert)
         
-        alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: ""), style: .default))
+       // alert.addAction(UIAlertAction(title: NSLocalizedString("OK", comment: ""), style: .default))
+        
+        let action = UIAlertAction(title: "OK", style: .default) { (alertOk) in
+            if self.interstitial.isReady {
+                self.interstitial.present(fromRootViewController: self)
+            }
+        }
+        
+        alert.addAction(action)
+        
         self.present(alert, animated: true, completion: nil)
+        
     }
     
     
     @IBAction func galleryPickerButton(_ sender: UIButton) {
         
-        choosePhotoLabel.isHidden = true
-       originalHorizonFlip = true
-      originalVertiaclFlip = true
+       
         
         imagePicker.sourceType = .photoLibrary
         present(imagePicker, animated: true) {
-            self.choosePhotoLabel.isHidden = true
+            self.fromGallery = true
+            self.chooseButton.isHidden = true
             self.originalHorizonFlip = true
             self.originalVertiaclFlip = true
+            self.bannerFunc()
+            self.interstitialAds()
         }
         
   
         
     }
     
-    @IBAction func cameraPickerButton(_ sender: UIButton) {
-        
-        choosePhotoLabel.isHidden = true
-        originalHorizonFlip = true
-        originalVertiaclFlip = true
-        
-        imagePicker.sourceType = .camera
-        present(imagePicker, animated: true) {
-            self.choosePhotoLabel.isHidden = true
-            self.originalHorizonFlip = true
-            self.originalVertiaclFlip = true
-        }
-
-        
-    }
+   
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
         
+        
+      
+        
         if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
-            originalImage = image
-            imageView.image = image
+           originalImage = UIImage(cgImage: image.cgImage!, scale: 1.0, orientation: .up)
+            //originalImage = image
+            imageView.image = originalImage
+            fromGallery = nil
         }
         
         dismiss(animated: true, completion: nil)
         
-    }
+        }
+            
+    
+    
     
     func imagePickerControllerDidCancel(_ picker: UIImagePickerController) {
         dismiss(animated: true, completion: nil)
@@ -159,8 +187,30 @@ class ViewController: UIViewController , UIImagePickerControllerDelegate,UINavig
             }} else {
             alert()
         }
-  
+ 
     }
+    
+    
+//    @IBAction func rotationLeft(_ sender: UIButton) {
+//        if originalImage != nil {
+//                originalImage = UIImage(cgImage: originalImage!.cgImage!, scale: 1.0, orientation: .left)
+//                imageView.image = originalImage
+//            }
+//        
+//        
+//    }
+//    
+//    
+//    
+//    @IBAction func rotationRight(_ sender: UIButton) {
+//        
+//        if originalImage != nil {
+//            originalImage = UIImage(cgImage: originalImage!.cgImage!, scale: 1.0, orientation: .right)
+//            imageView.image = originalImage
+//        }
+//        
+//    }
+    
     
     
     func alert() {
